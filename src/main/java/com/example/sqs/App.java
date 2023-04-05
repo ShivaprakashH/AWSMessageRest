21a49ded996671a4ca9e410648b1459085607ec8
ShivaprakashHurukadli <78000399+ShivaprakashHurukadli@users.noreply.github.com>
2023-04-05 15:26:24 -0500

    new project

---
+++ src/main/java/com/example/sqs/App.java
@@ -0,0 +1,11 @@
+package com.example.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/example/sqs/MainController.java
@@ -0,0 +1,57 @@
+package com.example.sqs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.UUID;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/chat")
+public class MainController {
+
+    @Autowired
+    SendReceiveMessages msgService;
+
+    // Add a new message.
+    @PostMapping("/add")
+    List<MessageData> addItems(HttpServletRequest request, HttpServletResponse response) {
+        String user = request.getParameter("user");
+        String message = request.getParameter("message");
+
+        // Generate the ID.
+        UUID uuid = UUID.randomUUID();
+        String msgId = uuid.toString();
+
+        MessageData messageOb = new MessageData();
+        messageOb.setId(msgId);
+        messageOb.setName(user);
+        messageOb.setBody(message);
+        msgService.processMessage(messageOb);
+        return msgService.getMessages();
+    }
+
+    // Purge the queue.
+    @RequestMapping(value = "/purge", method = RequestMethod.GET)
+    @ResponseBody
+    String purgeMessages(HttpServletRequest request, HttpServletResponse response) {
+        msgService.purgeMyQueue();
+        return "Queue is purged";
+    }
+
+    // Get messages.
+    @RequestMapping(value = "/msgs", method = RequestMethod.GET)
+    @ResponseBody
+    List<MessageData> getItems(HttpServletRequest request, HttpServletResponse response) {
+        List<MessageData> data =  msgService.getMessages();
+        return data;
+    }
+}
+
